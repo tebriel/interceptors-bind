2abb8f865bf4111c8984d5c9121ebf25028bb668
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2024-09-10 18:24:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# @mswjs/interceptors bind issue repro
---
+++ devenv.nix
@@ -0,0 +1,50 @@
+{ pkgs, lib, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.openssl ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
+  dotenv.enable = true;
+
+  # cachix.pull = [ "pre-commit-hooks" ];
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep "2.42.0"
+  # '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.javascript = {
+    enable = true;
+
+    package = pkgs.nodejs_22;
+    npm = {
+      enable = true;
+      package = pkgs.nodejs_22;
+      install.enable = true;
+    };
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
